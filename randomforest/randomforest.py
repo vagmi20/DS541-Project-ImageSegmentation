18c5b3e0bc86230ab4762680f2da68a760665562
danile981199 <63995138+danile981199@users.noreply.github.com>
2024-12-01 23:19:40 -0500

    Update randomforest.py

--- randomforest/randomforest.py
+++ randomforest/randomforest.py
@@ -1,1 +1,18 @@
+import tensorflow as tf
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
+
+
+
+# 1. Normalize pixel values to be between 0 and 1
+train_images_normalized = train_images / 255.0
+test_images_normalized = test_images / 255.0
